ecfba10a618dc7422448b7f19747337c48ac8ae9
Zouhair Fouiguira <Zouhairfgra@gmail.com>
2025-07-23 16:27:18 +0100

    add gluestack

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { ReduxProvider } from 'app/store/ReduxProvider';
 import { MainAppNavigator } from 'app/navigation/MainAppNavigator';
 import './global.css';
 
 export default function App() {
   return (
-    <ReduxProvider>
-      <MainAppNavigator />
-    </ReduxProvider>
+    <GluestackUIProvider mode="light"><ReduxProvider>
+        <MainAppNavigator />
+      </ReduxProvider></GluestackUIProvider>
   );
 }
